c3cf64a95ee22c31e483025236570e94e6a7a586
Frank Feuerbacher <frank@feuerbacher.us>
2020-06-22 02:17:58 -0500

    Nit- minor formatting

--- lib/windows/busydialog.py
+++ lib/windows/busydialog.py
@@ -5,19 +5,19 @@ from lib import util
 
 class BusyDialogReader(WindowReaderBase):
     ID = 'busydialog'
-    
+
     def init(self):
         self.next = 0
         self.play()
-        
+
     def play(self):
         duration = util.playSound('busy',return_duration=True)
         self.next = time.time() + duration
-        
+
     def getMonitoredText(self,isSpeaking=False):
         now = time.time()
         if now > self.next:
             self.play()
-            
+
     def close(self):
         util.stopSounds()
