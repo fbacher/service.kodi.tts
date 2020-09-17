a41081053cdf74ed63850d25ea9678a60877136a
Frank Feuerbacher <frank@feuerbacher.us>
2020-09-17 08:34:33 -0500

    Add ability to cache generated audio

--- resources/lib/cache/voicecache.py
+++ resources/lib/cache/voicecache.py
@@ -1,55 +1,84 @@
+# -*- coding: utf-8 -*-
+
 import datetime
 import hashlib
 import io
 import os
 import time
-import zlib
-from lib import util
+import xbmcvfs
 
-import xbmc
+from common.configuration_utils import ConfigUtils
+from common.constants import Constants
+from common.logger import LazyLogger
+from common.settings import Settings
+
+
+if Constants.INCLUDE_MODULE_PATH_IN_LOGGER:
+    module_logger = LazyLogger.get_addon_module_logger().getChild(
+        'lib.cache')
+else:
+    module_logger = LazyLogger.get_addon_module_logger()
 
 
 class VoiceCache:
 
-    @staticmethod
-    def is_cache_sound_files():
-        return util.getSetting('cache_voice_files', False)
+    _logger = None
+    ignore_cache_count = 0
 
-    @staticmethod
-    def get_sound_file(text_to_voice, suffix):
-        if not VoiceCache.is_cache_sound_files():
-            return None
+    def __init__(self):
+        VoiceCache._logger = module_logger.getChild(
+            self.__class__.__name__)  # type: LazyLogger
 
+    @classmethod
+    def for_debug_setting_changed(cls):
+        #
+        # For debugging. Reduces load on remote service while experimenting.
+        # Remove on shipping product.
+        #
+        # Causes cache to be ignored for five voicings after any backend setting
+        # has changed
+
+        cls.ignore_cache_count = 0
+
+    @classmethod
+    def is_cache_sound_files(cls, backend_class):
+        if Settings.configuring_settings() and cls.ignore_cache_count < 3:
+            cls.ignore_cache_count += 1
+            return False
+
+        return backend_class.getSetting(Settings.CACHE_SPEECH)
+
+    @classmethod
+    def get_sound_file_path(cls, text_to_voice, suffix):
         path = VoiceCache.get_path_to_voice_file(text_to_voice, suffix)
+
         exception_occurred = False
-        voiced_text = None
+        file_exists = False
         try:
             if not os.path.exists(path):
-                return None
+                cls._logger.debug_verbose(
+                    'Not cached: {} {}'.format(text_to_voice, path))
+                return path, file_exists
 
             if not os.access(path, os.R_OK):
-                util.VERBOSE_LOG(
+                cls._logger.debug_verbose(
                     'VoiceCache.get_sound_file- Can not read cache file: {}'.format(path))
-                return None
+                return path, file_exists
 
             expiration_time = time.time() - \
-                datetime.timedelta(util.getSetting(
-                    'cache_expiration_days', 30)).total_seconds()
+                datetime.timedelta(Settings.getSetting(
+                    Settings.CACHE_EXPIRATION_DAYS,
+                    Settings.CACHE_EXPIRATION_DEFAULT)).total_seconds()
 
             if os.stat(path).st_mtime < expiration_time:
-                util.VERBOSE_LOG(
+                cls._logger.debug_verbose(
                     'VoiceCache.get_sound_file- Expired sound file: {}'.format(path))
-                return None
+                return path, file_exists
 
-            with io.open(path, mode='rb') as cache_file:
-                voiced_text = cache_file.read()
-        except IOError as e:
-            util.ERROR('IOError reading voice file: {}'.format(path))
-            util.ERROR(str(e))
-            exception_occurred = True
         except Exception as e:
-            util.ERROR('Exception reading voice file: {}'.format(path))
-            util.ERROR(str(e))
+            cls._logger.error(
+                'Exception accessing voice file: {}'.format(path))
+            cls._logger.error(str(e))
             exception_occurred = True
 
         try:
@@ -57,32 +86,60 @@ class VoiceCache:
             if exception_occurred and path is not None:
                 os.remove(path)
         except Exception as e:
-            util.ERROR('Trying to delete bad cache file.')
-        return voiced_text
+            cls._logger.error('Trying to delete bad cache file.')
+
+        file_exists = True
+        return path, file_exists
+
+    @classmethod
+    def get_text_to_speech(cls, text_to_voice, suffix):
+        path, exists = cls.get_sound_file_path(text_to_voice, suffix)
+
+        exception_occurred = False
+        voiced_text = None
+        if exists:
+            try:
+                with io.open(path, mode='rb') as cache_file:
+                    voiced_text = cache_file.read()
+            except IOError as e:
+                cls._logger.error(
+                    'IOError reading voice file: {}'.format(path))
+                cls._logger.error(str(e))
+                exception_occurred = True
+            except Exception as e:
+                cls._logger.error(
+                    'Exception reading voice file: {}'.format(path))
+                cls._logger.error(str(e))
+                exception_occurred = True
+
+            try:
+                # Blow away bad cache file
+                if exception_occurred and path is not None:
+                    os.remove(path)
+            except Exception as e:
+                cls._logger.error('Trying to delete bad cache file.')
+
+        if voiced_text is not None:
+            cls._logger.debug_verbose(
+                'found: {} for: {}'.format(path, text_to_voice))
+        return path, voiced_text
 
     @staticmethod
     def get_path_to_voice_file(text_to_voice, suffix):
         file_name = VoiceCache.get_hash(text_to_voice) + suffix
-        cache_directory = util.getSetting('cache_path')
-        cache_directory = xbmc.translatePath(cache_directory)
+        cache_directory = Settings.getSetting(Settings.CACHE_PATH)
+        cache_directory = xbmcvfs.translatePath(cache_directory)
         path = os.path.join(cache_directory, file_name)
         return path
 
     @staticmethod
     def get_hash(text_to_voice):
         hash_value = hashlib.md5(text_to_voice.encode('UTF-8')).hexdigest()
-        # if len(text_to_voice) > 5:
-        #    hash_value = text_to_voice[:4]
-        #    hash_value = hash_value + '_' + \
-        #        hashlib.md5(text_to_voice.encode('UTF-8')).hexdigest()
-        #
-        # else:
-        #     hash_value = text_to_voice
 
         return hash_value
 
-    @staticmethod
-    def save_sound_file(text_to_voice,  # type: str
+    @classmethod
+    def save_sound_file(cls, text_to_voice,  # type: str
                         suffix,  # type: str
                         voiced_text  # type: bytes
                         ):
@@ -91,37 +148,41 @@ class VoiceCache:
             Write the given voiced text to the cache
         """
         path = VoiceCache.get_path_to_voice_file(text_to_voice, suffix)
-        cache_directory = util.getSetting('cache_path',
-                                          xbmc.translatePath('special://userdata/{}/cache'.format(util.ADDON_ID)))
+        cache_directory = Settings.getSetting(Settings.CACHE_PATH,
+                                              Constants.DEFAULT_CACHE_DIRECTORY)
         try:
             if not os.path.exists(cache_directory):
                 try:
                     os.makedirs(cache_directory)
                 except Exception as e:
-                    util.ERROR(
+                    cls._logger.error(
                         'Can not create cache directory: {}'.format(cache_directory))
 
             if os.path.exists(path) and not os.access(path, os.W_OK):
-                util.ERROR('Can not write cache file: {}'.format(path))
+                cls._logger.error('Can not write cache file: {}'.format(path))
             with io.open(path, mode='wb') as cacheFile:
                 cacheFile.write(voiced_text)
                 cacheFile.flush()
         except Exception as e:
-            util.ERROR('Can not write cache file: {}'.format(path))
+            cls._logger.error('Can not write cache file: {}'.format(path))
 
-    @staticmethod
-    def clean_cache(purge=False):
+    @classmethod
+    def clean_cache(cls, purge=False):
+        return
         dummy_cached_file = VoiceCache.get_path_to_voice_file('', '')
         cache_directory, _ = os.path.split(dummy_cached_file)
         expiration_time = time.time() - \
-            util.getSetting(
+            Settings.getSetting(
                 'cache_expiration_days', 30) * 86400
         for root, dirs, files in os.walk(cache_directory, topdown=False):
             for file in files:
                 try:
                     path = os.path.join(root, file)
                     if purge or os.stat(path).st_mtime < expiration_time:
-                        util.VERBOSE_LOG('Deleting: {}'.format(path))
+                        cls._logger.debug_verbose('Deleting: {}'.format(path))
                         os.remove(path)
                 except Exception as e:
                     pass
+
+
+instance = VoiceCache()
