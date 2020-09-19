4bf42eaf7a198697d53425fc900df700d9e7bc0f
Frank Feuerbacher <frank@feuerbacher.us>
2020-09-19 11:01:19 -0500

    Add logging

--- resources/lib/utils/addoninfo.py
+++ resources/lib/utils/addoninfo.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import os, hashlib, json
 import xbmc
-from . import util
 
-DATAPATH = os.path.join(util.profileDirectory(),'addon_data.json')
+from common.constants import Constants
+from common.logger import LazyLogger
+#from common.old_logger import OldLogger
+from common.settings import Settings
+
+# TODO: What is this for?
+
+module_logger = LazyLogger.get_addon_module_logger(file_path=__file__)
+
+DATAPATH = os.path.join(Constants.PROFILE_PATH,'addon_data.json')
 BASE = '{ "jsonrpc": "2.0", "id": 1, "method": "Addons.GetAddons", "params": {"enabled": true,"properties": ["name","version"]}}'
 NEW_VERSIONS = False
 
@@ -11,12 +19,12 @@ def getAddonsMD5():
     return hashlib.md5(xbmc.executeJSONRPC(BASE).encode('utf-8')).hexdigest()
 
 def saveAddonsMD5(md5):
-    util.VERBOSE_LOG('addoninfo.saveAddonsMD5')
-    util.setSetting('addons_MD5',md5)
+    module_logger.debug_verbose('addoninfo.saveAddonsMD5')
+    Settings.setSetting('addons_MD5',md5)
 
 def loadAddonsMD5():
-    util.VERBOSE_LOG('addoninfo.loadAddonsMD5')
-    return util.getSetting('addons_MD5')
+    module_logger.debug_verbose('addoninfo.loadAddonsMD5')
+    return Settings.getSetting('addons_MD5')
 
 def initAddonsData(force=False):
     if not force and loadAddonsMD5() and os.path.exists(DATAPATH): return
